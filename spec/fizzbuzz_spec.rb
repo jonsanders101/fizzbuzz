69e1303af74874753cc050fa10bae049c4ec243e
Jon Sanders <jonsanders101@aol.com>
2017-09-21 23:27:22 +0100

    Fizzbuzz works for multiples of 15

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -9,11 +9,15 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
 
-  it 'returns "fizz" when passed any mutliple of 3' do
+  it 'returns "fizz" when passed any multiple of 3' do
     expect(fizzbuzz(3 * rand(1..100))).to eq 'fizz'
   end
 
-  it 'returns "buzz" when passed any mutliple of 5' do
+  it 'returns "buzz" when passed any multiple of 5' do
     expect(fizzbuzz(5 * rand(1..100))).to eq 'buzz'
+  end
+
+  it 'returns "fizzbuzz" when passed any multiple of 3 and 5' do
+    expect(fizzbuzz(15 * rand(1..100))).to eq 'fizzbuzz'
   end
 end
